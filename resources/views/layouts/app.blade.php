53b4bdc4615f4a33adf14c8990dd005ea4f66a08
Oleh Talanoff <talanov.o@gmail.com>
2018-10-25 13:10:29 +0300

    favicons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,10 +8,21 @@
     <title>{{ config('app.name', 'IB') . (isset($app_title) ? ' | ' . $app_title : '') }}</title>
 
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans:400,400i,700,700i&amp;subset=cyrillic" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans:400,400i,700,700i&amp;subset=cyrillic"
+          rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicons/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicons/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('images/favicons/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('images/favicons/safari-pinned-tab.svg') }}" color="#5bbad5">
+    <link rel="shortcut icon" href="{{ asset('images/favicons/favicon.ico') }}">
+    <meta name="apple-mobile-web-app-title" content="Impression Bureau">
+    <meta name="application-name" content="Impression Bureau">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="{{ asset('images/favicons/browserconfig.xml') }}">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body class="loading">
 @include('partials.app.loader')
@@ -24,5 +35,7 @@
     </main>
     @include('partials.app.footer')
 </div>
+
+<script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
