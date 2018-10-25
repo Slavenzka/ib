fa2421a1b2e0178b3d172e0c4a2190eabb3b552f
Oleh Talanoff <talanov.o@gmail.com>
2018-10-25 11:54:05 +0300

    works page

--- app/Http/Controllers/Front/WorkController.php
+++ app/Http/Controllers/Front/WorkController.php
@@ -7,6 +7,12 @@ use App\Models\Work\Work;
 
 class WorkController extends Controller
 {
+    public function index()
+    {
+        return \view('app.work.index', [
+            'works' => Work::latest()->get()
+        ]);
+    }
     /**
      * @param Work $work
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
---
+++ resources/views/app/work/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app', ['app_title' => trans('page.title.works')])
+
+@section('content')
+
+    <section id="content">
+        <div class="container">
+            <h1 class="page-title">
+                <revealer>
+                    @lang('page.title.works')
+                </revealer>
+            </h1>
+
+            <div class="grid works">
+            @forelse($works as $work)
+                <div class="w-md-6/12">
+                    <article class="work-article">
+                        <div class="work-article__img-wrap">
+                            <a href="{{ route('app.work.show', $work) }}" class="work-article__link"></a>
+                            <div class="work-article__img" style="background-image: url({{ $work->image_medium }});"></div>
+                        </div>
+                        <div class="work-article__side">{{ $work->type->title }}</div>
+                        <div class="work-article__title-wrap">
+                            <span class="work-article__number">{{ $loop->iteration }}</span>
+                            <h3 class="work-article__title">{{ $work->title }}</h3>
+                            <h4 class="work-article__subtitle mb-0">{{ $work->description }}</h4>
+                        </div>
+                    </article>
+                </div>
+            @empty
+                <div class="w-1">...</div>
+            @endforelse
+            </div>
+        </div>
+    </section>
+
+@endsection
