175641bc57ee446a0b7a99378c2c74d5d9cca929
Oleh Talanoff <talanov.o@gmail.com>
2018-11-19 11:49:28 +0200

    #

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,8 @@
     <meta property="og:url" content="{{url()->current()}}"/>
     <meta property="og:type" content="article"/>
     <meta property="og:image" content="{{ asset('images/favicons/apple-touch-icon.png') }}"/>
-    <meta property="og:image:width" content="180">
-    <meta property="og:image:height" content="180">
+    <meta property="og:image:width" content="200">
+    <meta property="og:image:height" content="200">
 
     <meta name="description"
           content='Impression Bureau — создание уникальных сайтов. Магазины, Лендинги, Корпоративные.'>
