8b9059a58169943b2925d9154664169e43842dab
Oleh Talanoff <talanov.o@gmail.com>
2018-12-13 12:27:38 +0200

    Add mail send when brief enrolled

--- app/Mail/BriefFilled.php
+++ app/Mail/BriefFilled.php
@@ -5,39 +5,35 @@ namespace App\Mail;
 use App\Models\Contact\Brief;
 use App\Models\User\User;
 use Carbon\Carbon;
-use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class BriefFilled extends Mailable
 {
-    use Queueable, SerializesModels;
-    /**
-     * @var Brief
-     */
-    public $brief;
+	/**
+	 * @var Brief
+	 */
+	public $brief;
 
 	/**
 	 * Create a new message instance.
 	 *
 	 * @param Brief $brief
 	 */
-    public function __construct(Brief $brief)
-    {
-        $this->brief = $brief;
-    }
+	public function __construct(Brief $brief)
+	{
+		$this->brief = $brief;
+	}
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
-    public function build()
-    {
-        return $this
-            ->to(User::getUsersEmailsByRoles())
-            ->subject('Новый бриф. ' . Carbon::now()->format('d.m.Y H:i'))
-            ->view('mail.brief');
-    }
+	/**
+	 * Build the message.
+	 *
+	 * @return $this
+	 */
+	public function build()
+	{
+		return $this
+			->to(User::getUsersEmailsByRoles())
+			->subject('Новый бриф. ' . Carbon::now()->format('d.m.Y H:i'))
+			->view('mail.brief');
+	}
 }
--- app/Mail/ContactFilled.php
+++ app/Mail/ContactFilled.php
@@ -5,14 +5,10 @@ namespace App\Mail;
 use App\Models\Contact\Contact;
 use App\Models\User\User;
 use Carbon\Carbon;
-use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
 class ContactFilled extends Mailable
 {
-//    use Queueable, SerializesModels;
 	/**
 	 * @var Contact
 	 */
@@ -23,22 +19,22 @@ class ContactFilled extends Mailable
 	 *
 	 * @param Contact $contact
 	 */
-    public function __construct(Contact $contact)
-    {
+	public function __construct(Contact $contact)
+	{
 		$this->contact = $contact;
 	}
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
-    public function build()
-    {
-        return $this
+	/**
+	 * Build the message.
+	 *
+	 * @return $this
+	 */
+	public function build()
+	{
+		return $this
 			->to(User::getUsersEmailsByRoles())
 			->replyTo($this->contact->email)
 			->subject('Новый контакт. ' . Carbon::now()->format('d.m.Y H:i'))
 			->view('mail.contact');
-    }
+	}
 }
