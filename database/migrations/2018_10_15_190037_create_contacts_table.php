b2a17c8df2520c5f3b3f3c618c805e6a401978b4
Oleh Talanoff <talanov.o@gmail.com>
2018-12-13 11:24:48 +0200

    Add mail send when brief enrolled

--- database/migrations/2018_10_15_190037_create_contacts_table.php
+++ database/migrations/2018_10_15_190037_create_contacts_table.php
@@ -19,6 +19,7 @@ class CreateContactsTable extends Migration
             $table->string('phone');
             $table->string('email');
             $table->text('message')->nullable();
+            $table->text('comment')->nullable();
             $table->enum('status', array_keys(App\Models\Contact\Contact::$STATUSES))
                   ->default(array_keys(App\Models\Contact\Contact::$STATUSES)[0]);
             $table->timestamps();
