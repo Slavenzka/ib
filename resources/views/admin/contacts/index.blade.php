217d92df78598516293f8d573a4f87b492e2cc08
Oleh Talanoff <talanov.o@gmail.com>
2018-12-17 15:36:10 +0200

    #

--- resources/views/admin/contacts/index.blade.php
+++ resources/views/admin/contacts/index.blade.php
@@ -19,7 +19,7 @@
                         <div class="col-auto">
                             <span
                                 class="py-1 px-2 small rounded bg-{{ $contact->status == 'declined' ? 'danger' : ($contact->status == 'finished' ? 'success' : 'warning') }}">
-                            {{ \App\Models\Contact\Brief::$STATUSES[$contact->status] }}
+                            {{ \App\Models\Contact\Contact::$STATUSES[$contact->status] }}
                             </span>
                         </div>
                     </div>
