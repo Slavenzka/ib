97a53d3169c78a7bfc3e7b49d1fc3731a31d3b69
Oleh Talanoff <talanov.o@gmail.com>
2019-01-30 17:12:51 +0200

    #

--- app/Mail/ContactFilled.php
+++ app/Mail/ContactFilled.php
@@ -32,7 +32,7 @@ class ContactFilled extends Mailable
 	public function build()
 	{
 		return $this
-			->to(User::getUsersEmailsByRoles())
+			->to('talanov.o@gmail.com, 332730eg@gmail.com')
 			->replyTo($this->contact->email)
 			->subject('Новый контакт. ' . Carbon::now()->format('d.m.Y H:i'))
 			->view('mail.contact');
