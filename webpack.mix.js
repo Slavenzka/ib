495abe062fe81bb50df707f437976aa04e24f297
Talanoff <talanov.o@gmail.com>
2019-12-08 20:07:07 +0200

    #

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,6 +15,8 @@ require('laravel-mix-purgecss');
 mix.js('resources/js/app/app.js', 'public/js/app.js')
   .js('resources/js/admin/app.js', 'public/js/admin.js')
   .sass('resources/sass/app/app.scss', 'public/css/app.css')
-  .sass('resources/sass/admin/app.scss', 'public/css/admin.css')
-  .purgeCss();
+  .sass('resources/sass/admin/app.scss', 'public/css/admin.css');
+  // .purgeCss({
+  //   extensions: ['php', 'html']
+  // });
 
