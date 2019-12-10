0852ebc235f81dc90b8aa772560b2f77241a30b5
Talanoff <talanov.o@gmail.com>
2019-12-10 13:10:29 +0200

    #

--- resources/views/app/home/promo.blade.php
+++ resources/views/app/home/promo.blade.php
@@ -2,22 +2,31 @@
     <div class="container text-center">
         <h1 class="section-title">
             <revealer class="inline-flex" :params="{direction: 'rl'}">
-                @lang('page.home.promo.title')
+                {{ __('page.home.promo.title') }}
             </revealer>
         </h1>
 
         <h2 class="section-title--sm">
             <revealer class="text-light" :params="{direction: 'lr', bgcolor: '#353535'}">
-                @lang('page.home.promo.subtitle')
+                {{ __('page.home.promo.subtitle') }}
             </revealer>
         </h2>
 
-        <div class="mt-10">
-            <revealer :params="{direction: 'bt'}">
-                <a href="{{ route('app.brief.index') }}" class="button button--primary">
-                    @lang('buttons.discuss')
-                </a>
-            </revealer>
+        <div class="mt-8 flex justify-center">
+            <div class="mx-4 my-2">
+                <revealer :params="{direction: 'rl'}">
+                    <a href="{{ route('app.brief.index') }}" class="button button--primary button--xl">
+                        {{ __('buttons.fill_brief') }}
+                    </a>
+                </revealer>
+            </div>
+            <div class="mx-4 my-2">
+                <revealer :params="{direction: 'lr'}">
+                    <a href="#contact-us" class="button button--primary button--xl">
+                        {{ __('buttons.discuss') }}
+                    </a>
+                </revealer>
+            </div>
         </div>
     </div>
 </section>
