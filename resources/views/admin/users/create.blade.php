28bf60cd6341ef665ed116d95d207a684edc9875
Talanoff <talanov.o@gmail.com>
2019-12-10 21:10:50 +0200

    Telegram added. Refactor.

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -7,23 +7,28 @@
 
         <div class="form-group">
             <label for="name">Имя</label>
-            <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}" required>
+            <input type="text" name="name" id="name" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
+                   value="{{ old('name') }}" required autofocus>
         </div>
 
         <div class="form-group">
             <label for="email">Email</label>
-            <input type="text" name="email" id="email" class="form-control" value="{{ old('email') }}" required>
+            <input type="text" name="email" id="email"
+                   class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" value="{{ old('email') }}"
+                   required>
         </div>
 
         <div class="form-group">
             <label for="password">Пароль</label>
-            <input type="password" name="password" id="password" class="form-control" value="{{ old('password') }}" required>
+            <input type="password" name="password" id="password"
+                   class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" value="{{ old('password') }}"
+                   required autocomplete="nope">
         </div>
 
         <div class="form-group">
             <label for="telegram_user_id">Telegram ID</label>
             <input type="text" name="telegram_user_id" id="telegram_user_id" class="form-control"
-                   value="{{ old('telegram_user_id') }}">
+                   value="{{ old('telegram_user_id') }}" autocomplete="nope">
         </div>
 
         <div class="mt-4">
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -8,19 +8,22 @@
 
         <div class="form-group">
             <label for="name">Имя</label>
-            <input type="text" name="name" id="name" class="form-control" value="{{ old('name') ?? $user->name }}"
-                   required>
+            <input type="text" name="name" id="name" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
+                   value="{{ old('name') ?? $user->name }}" required>
         </div>
 
         <div class="form-group">
             <label for="email">Email</label>
-            <input type="text" name="email" id="email" class="form-control" value="{{ old('email') ?? $user->email }}"
+            <input type="text" name="email" id="email"
+                   class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                   value="{{ old('email') ?? $user->email }}"
                    readonly disabled>
         </div>
 
         <div class="form-group">
             <label for="password">Пароль</label>
-            <input type="password" name="password" id="password" class="form-control"
+            <input type="password" name="password" id="password"
+                   class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
                    value="{{ old('password') }}" required>
         </div>
 
