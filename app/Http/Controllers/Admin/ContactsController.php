aaf339bcf46b8198ac0fb2ded13558148f1832c0
Talanoff <talanov.o@gmail.com>
2019-12-11 11:14:56 +0200

    Telegram added. Refactor.

--- app/Http/Controllers/Admin/ContactsController.php
+++ app/Http/Controllers/Admin/ContactsController.php
@@ -4,27 +4,54 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Contact;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use function back;
 use function view;
 
 class ContactsController extends Controller
 {
-    public function index()
+    /**
+     * @return View
+     */
+    public function index(): View
     {
         return view('admin.contacts.index', [
             'contacts' => Contact::latest()->paginate(50),
         ]);
     }
 
-    public function edit(Contact $contact)
+    /**
+     * @param Contact $contact
+     * @return View
+     */
+    public function edit(Contact $contact): View
     {
         return view('admin.contacts.edit', compact('contact'));
     }
 
-    public function update(Request $request, Contact $contact)
+    /**
+     * @param Request $request
+     * @param Contact $contact
+     * @return RedirectResponse
+     */
+    public function update(Request $request, Contact $contact): RedirectResponse
     {
-        $contact->update($request->only('name', 'phone', 'email', 'comment', 'status'));
-        return back();
+        $contact->update($request->all());
+
+        return back()->with('success', 'Контакт обновлен.');
+    }
+
+    /**
+     * @param Contact $contact
+     * @return RedirectResponse
+     * @throws \Exception
+     */
+    public function destroy(Contact $contact): RedirectResponse
+    {
+        $contact->delete();
+
+        return back()->with('success', 'Контакт удален.');
     }
 }
